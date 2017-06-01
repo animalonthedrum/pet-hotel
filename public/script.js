fbc7dad727a16eed5aa87eb8b34198d9241fee3d
Brett Miller <miller.brett.andrew@gmail.com>
2017-06-01 14:59:02 -0500

    adding table

--- public/script.js
+++ public/script.js
@@ -1,58 +1,83 @@
-$(document).ready(function(){
+$(document).ready(function() {
   console.log('JQ');
 
-$('#registerOwner').on('click', ownerName);
-$('#addPet').on('click', petInfo);
-dropDown();
+  $('#registerOwner').on('click', ownerName);
+  $('#addPet').on('click', petInfo);
+  dropDown();
+  tableTime();
 
+}); // end on ready
 
-});// end on ready
-
-var ownerName = function(){
+var ownerName = function() {
   var objectToSend = {
     name: $('#firstName').val() + ' ' + $('#lastName').val()
   };
-$.ajax({
-  type: 'POST',
-  url: '/owner',
-  data: objectToSend,
-  success: function(response){
-    console.log('woof:', response);
-  }//end success
-});//end ajax post
-dropDown();
-};// end owner function
+  $.ajax({
+    type: 'POST',
+    url: '/owner',
+    data: objectToSend,
+    success: function(response) {
+      console.log('woof:', response);
+    } //end success
+  }); //end ajax post
+  dropDown();
+}; // end owner function
 
-var dropDown = function(){
-  $.ajax ({
+var dropDown = function() {
+  $.ajax({
     type: 'GET',
     url: '/owner',
-    success: function(response){
+    success: function(response) {
       console.log('meow', response);
-        $('.owners').empty();
-    for(var i = 0; i<response.length; i++){
-    $('#owners').append('<option>' + response[i].owner + '</option>');
-}
-}
-  });//
+      $('#owners').empty();
+      for (var i = 0; i < response.length; i++) {
+        $('#owners').append('<option>' + response[i].owner + '</option>');
+      }
+    }
+  }); //
 
 
 };
 
-var petInfo = function(){
+var petInfo = function() {
   var petToSend = {
     name: $('#petName').val(),
     color: $('#color').val(),
     breed: $('#breed').val(),
-    owner:$('#owners').val()
-};// end petToSend
-console.log(petToSend);
-$.ajax({
-  type: 'POST',
-  url: '/pet',
-  data: petToSend,
-  success: function(response){
-    console.log('ribbet:', response);
-  }//end success
-});//end ajax post
+    owner: $('#owners').val()
+  }; // end petToSend
+  console.log(petToSend);
+  $.ajax({
+    type: 'POST',
+    url: '/pet',
+    data: petToSend,
+    success: function(response) {
+      console.log('ribbet:', response);
+    } //end success
+  }); //end ajax post
 };
+
+var tableTime = function() {
+  $.ajax({
+    type: 'GET',
+    url: '/owner',
+    success: function(response) {
+      console.log('meow', response);
+      $('#owners').empty();
+
+
+      for (var i = 0; i < response.length; i++) {
+        var tableRow = '<tr>';
+        tableRow += '<td>' + response[i].owner + '</td>';
+        tableRow += '<td>' + response[i].pet + '</td>';
+        tableRow += '<td>' + response[i].breed + '</td>';
+        tableRow += '<td>' + response[i].color + '</td>';
+        tableRow += '<td><button>GO</button></td>';
+        tableRow += '<td><button>GO</button></td>';
+        tableRow += '<td><button>OUT</button></td>';
+        tableRow += '</tr>';
+        $('#myTable tr:last').after(tableRow);
+      }
+    }
+  }); //
+}; //end tabelTime
--- server.js
+++ server.js
@@ -8,81 +8,83 @@ var pg = require('pg');
 //globals
 var port = 8080;
 var config = {
-  database:'pet_hotel',
+  database: 'pet_hotel',
   host: 'localhost',
   port: 5432,
   max: 30
 }; // end config obj
 
-var pool = new pg.Pool( config );
+var pool = new pg.Pool(config);
 
 //uses
-app.use(express.static( 'public' ) );
-app.use(bodyParser.urlencoded( {extended: true} ) );
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //spin up server
-app.listen(port, function(){
-  console.log('server is up on port:', port );
+app.listen(port, function() {
+  console.log('server is up on port:', port);
 });
 //base url
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   console.log('in base url');
-  res.sendFile(path.resolve( 'views/index.html') );
-});//end base url
+  res.sendFile(path.resolve('views/index.html'));
+}); //end base url
 
-app.get('/owner', function(req, res){
+app.get('/owner', function(req, res) {
   console.log('drop down');
-  pool.connect(function(err, connection, done){
-    if(err){
+  pool.connect(function(err, connection, done) {
+    if (err) {
       console.log('error');
       done();
       res.send(400);
-    }else {
+    } else {
       console.log('connected to db');
       var allOwners = [];
       var resultSet = connection.query('SELECT * FROM pet_table');
-      resultSet.on('row', function(row){
+      resultSet.on('row', function(row) {
         allOwners.push(row);
-      });//end resultSet
-      resultSet.on('end', function(){
+      }); //end resultSet
+      resultSet.on('end', function() {
         done();
         res.send(allOwners);
-      });//end end resultSet
+      }); //end end resultSet
 
     }
-  });//done pool get
-});//end get
+  }); //done pool get
+}); //end get
 
 
 
-app.post('/owner', function(req,res){
+app.post('/owner', function(req, res) {
   console.log('I like pets:', req.body);
-pool.connect(function(err, connection, done){
-if(err){
-  console.log('error');
-  done();
-  res.send(400);
-}else {
-  console.log('connected to database');
-  connection.query("INSERT INTO pet_table(Owner) values ($1)", [req.body.name]);
-  done();
-  res.send(200);
-}//end else
-});//end pool connect
-});// end post
-app.post('/pet', function(req,res){
+  pool.connect(function(err, connection, done) {
+    if (err) {
+      console.log('error');
+      done();
+      res.send(400);
+    } else {
+      console.log('connected to database');
+      connection.query("INSERT INTO pet_table(Owner) values ($1)", [req.body.name]);
+      done();
+      res.send(200);
+    } //end else
+  }); //end pool connect
+}); // end post
+app.post('/pet', function(req, res) {
   console.log('I like pets2:', req.body);
-  pool.connect(function(err, connection, done){
-  if(err){
-  console.log('error');
-  done();
-  res.send(400);
-  }else {
-  console.log('connected');
-  connection.query("UPDATE pet_table SET pet = $1, color = $2, breed = $3 WHERE owner= $4", [req.body.name,req.body.color,req.body.breed, req.body.owner]);
-  done();
-  res.send(200);
-  }//end else
-  });//end pool connect
+  pool.connect(function(err, connection, done) {
+    if (err) {
+      console.log('error');
+      done();
+      res.send(400);
+    } else {
+      console.log('connected');
+      connection.query("UPDATE pet_table SET pet = $1, color = $2, breed = $3 WHERE owner= $4", [req.body.name, req.body.color, req.body.breed, req.body.owner]);
+      done();
+      res.send(200);
+    } //end else
+  }); //end pool connect
 
-});//end post
+}); //end post
